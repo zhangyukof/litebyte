8f421eed2cbdd70a818a715ab635e290fe8b9b18
zhangyukof <zhangyu800@126.com>
2020-01-07 15:50:08 +0800

    更新Demo

---
+++ Unity/Assets/LiteByteDemo/Scripts/Types/ST_VarUTFType.cs
@@ -0,0 +1,14 @@
+﻿namespace LiteByte.Demo {
+
+    using System;
+    using ProtoBuf;
+
+    /// <summary> String Type </summary>
+    [Serializable][ProtoContract]
+    public struct ST_VarUTFType {
+
+        [ProtoMember(1)] public string str;
+
+    }
+
+}
